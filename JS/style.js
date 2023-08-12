a48b3eae1c645a4d2ec62f19b92209ccfe21f1a7
ahmed1ibrahim12 <ahmed1ibahim12@gmail.com>
2023-08-12 04:06:25 +0300

    code

---
+++ JS/style.js
@@ -0,0 +1,10 @@
+
+function show1(){
+var x = document.getElementById("comment-1")
+
+    if(x.style.display == "block"){
+        x.style.display == "none";
+    } else{
+        x.style.display == "block";
+    }
+}
