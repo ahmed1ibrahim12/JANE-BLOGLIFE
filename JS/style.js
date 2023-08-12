4594dcaa6d196f11adfeba94af5bab475404d24f
ahmed1ibrahim12 <ahmed1ibahim12@gmail.com>
2023-08-12 13:46:40 +0300

    aside 1 done

--- JS/style.js
+++ JS/style.js
@@ -1,10 +1,29 @@
+var x = document.getElementById("comment");
 
 function show1(){
-var x = document.getElementById("comment-1")
+    if (x.style.display=="none"){
+        x.style.display="block";
+    } else {
+        x.style.display="none";
+    }
+}
 
-    if(x.style.display == "block"){
-        x.style.display == "none";
-    } else{
-        x.style.display == "block";
+var y = document.getElementById("comment2");
+
+function show2(){
+    if (y.style.display=="none"){
+        y.style.display="block";
+    } else {
+        y.style.display="none";
+    }
+}
+
+var z = document.getElementById("comment3");
+
+function show3(){
+    if (z.style.display=="none"){
+        z.style.display="block";
+    } else {
+        z.style.display="none";
     }
 }
